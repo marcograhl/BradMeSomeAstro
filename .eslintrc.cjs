52e42843e20bb8f6fb80a5433a3fc99fc053d544
marcograhl <manograhl@gmail.com>
2022-09-07 09:36:01 +0000

    added th eslint prettier and typscript settings from astro

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier', 'no-only-tests'],
+    rules: {
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-this-alias': 'off',
+        'no-console': 'warn',
+        'prefer-const': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-only-tests/no-only-tests': 'error',
+    },
+    overrides: [
+        {
+            files: ['packages/**/test/*.js', 'packages/**/*.test.js'],
+            env: {
+                mocha: true,
+            },
+            globals: {
+                globalThis: false, // false means read-only
+            },
+            rules: {
+                'no-console': 'off',
+            },
+        },
+    ],
+};
